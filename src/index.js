e9a9e8a7a4360497a4fca5925f8984951b05ffb9
Juan Carlos <jcs1011@gmail.com>
2022-10-27 18:13:56 -0500

    inicio modelos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDom from 'react-dom/client';
 import {BrowserRouter as  Router } from "react-router-dom";
 import App from './App'; 
 
+const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+
 const root = ReactDom.createRoot(document.getElementById('root'));
 
 

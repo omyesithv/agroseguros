5f069a8e28bd1a0057d4fe48f473d6b9c333769f
omyesithv <108715681+omyesithv@users.noreply.github.com>
2022-11-18 11:25:22 -0500

    Revert "Se agrega HomeServicios a la ruta inicio"
    This reverts commit e32a0cd6cc7879d8803b39e5c716217c2bb49a7b.

--- Frontend/src/componentes/Menu.js
+++ Frontend/src/componentes/Menu.js
@@ -11,15 +11,12 @@ import EditarAgricultor from  "../paginas/EditarAgricultor";
 import Confirmacion from "../componentes/Confirmacion";
 import { Container, Row, Col } from "react-bootstrap";
 import Login from "../paginas/Login";
-import Pageservicios from "../paginas/serviciosHome";
 import VerAgricultor from "../paginas/VerAgricultor";
-import HomeServicios from "../paginas/serviciosHome.jsx";
 
  const Menu=() =>{
     return (
-      <div>
       <Container>
-        {/* <Layout /> */}
+        <Layout />
         <Routes>
           <Route>
             <Route path="eliminarAgricultor/:id" element={<EliminarAgricultor />} />
@@ -27,16 +24,12 @@ import HomeServicios from "../paginas/serviciosHome.jsx";
             <Route path="Agricultor" element={<ListaAgro />} />
             <Route exact path="modificar/:id" element={<EditarAgricultor />} />
             <Route exact path="confirmacion/:id" element={<Confirmacion />} />
-            <Route path="home" element={<Login />} />
+            <Route path="/" element={<Login />} />
             <Route path="VerAgricultor" element={<VerAgricultor />} />
-            <Route path="services" element={<Pageservicios />} />
             <Route path="*" element={<Nofound />} />
-            <Route path="inicio" element={<HomeServicios />} />
-
           </Route>
         </Routes>
       </Container>
-      </div>
     );  
 }
 

2f53bebdb7db962158303a835ef54c5887ed32bf
Juan Carlos <78104297+Juan31452@users.noreply.github.com>
2022-11-25 11:33:19 -0500

    Update HomeAgricultor.js

--- Frontend/src/paginas/agro/HomeAgricultor.js
+++ Frontend/src/paginas/agro/HomeAgricultor.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Form, Link } from 'react-router-dom';
 import VerAgricultor from '../VerAgricultor';
-import {LOGOUT, PRIVATE2, PRIVATE7} from '../../componentes/router/Path';
+import {LOGOUT,PRIVATE2} from '../../componentes/router/Path';
 import ListaAgricultor from '../agro/listaAgricultor';
 import ListaInversor from '../agro/listainversionista';
 import "../../css/home-agricultor.css";
 import { Container } from 'react-bootstrap';
 //import EditarAgro from  "./EditarAgricultor";
+import ContextoUsuario from '../../componentes/context/ContextoUsuario';
+
 
 const HomeAgricultor = () => {
+  const {usuario} = useContext(ContextoUsuario);
+  console.log(usuario)
   return (
     <div className='home-agricultor'> 
       <div>
-
+        <p>{usuario ? ` Hola ${usuario.email} ` : 'Bienvenido'}</p> 
+  
 
 
         <h2>Bienvenido a AgroSeguro</h2>
@@ -25,19 +30,7 @@ const HomeAgricultor = () => {
         <Link to={PRIVATE2} className='button'>
           Ver Inversionistas
         </Link>
-        <Link to={PRIVATE7} className='button'>
-          Ver Productos
+        <Link to="/HomeAgricultor/VerAgro" className='button'>
+          Mi Perfil
         </Link>
-       
-        
-         
-        
 
-      </div>
-
-      
-    </div>  
-  )
-}
-
-export default HomeAgricultor
